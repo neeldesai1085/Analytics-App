673344f6c5d6bacab973bfa68d1b22478fcdddae
Neel Desai <neeldesai1085@gmail.com>
2026-06-19 16:40:45 +0530

    Create frontend folder setup

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -12,9 +12,9 @@ app.use(express.json());
 app.use("/api", eventsRouter);
 
 const PORT = process.env.PORT!;
-const MONGO_URI = process.env.MONGODB_URI !;
+const MONGO_URI = process.env.MONGODB_URI!;
 
 mongoose.connect(MONGO_URI).then(() => {
-    console.log("MongoDB connected");
-    app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+  console.log("MongoDB connected");
+  app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 }).catch((e) => console.error("Mongo connection error", e));
